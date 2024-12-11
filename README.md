bd5540af30a3b5d7c9b2e524e46e1e9e4e8b00c8
Mahmoud Abu Sultan <mahmoodkns@hotmail.com>
2024-12-11 14:32:39 +0200

    initial

---
+++ README.md
@@ -0,0 +1,24 @@
+## Techs used
+1. HTML.
+2. Javascript and Typescript.
+3. Webpack for bundling.
+4. Tailwindcss for styling.
+
+## Installation
+```bash
+npm install
+```
+This command will install all necessary dependancies for building this website.
+
+## Run the website
+To run the website on port 3000, run the command:
+```bash
+npm run dev
+```
+
+## Build the website
+After you finish developing the website, you need to build files:
+```bash
+npm run build
+```
+Generated files must be put in public_hmtl/ folder in the host service in order to deploy the website.
---
+++ webpack.config.ts
@@ -0,0 +1,85 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+import "webpack-dev-server";
+import webpack from "webpack";
+
+const webpackConfig: webpack.Configuration = {
+	mode: "production",
+	entry: path.resolve(__dirname, "src/index.ts"),
+	output: {
+		filename: "bundle.[contenthash].js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+		assetModuleFilename: "[name][ext]",
+	},
+	resolve: {
+		extensions: [".ts", ".js"],
+	},
+	module: {
+		rules: [
+			{
+				// Test for both .js and .jsx files
+				test: /\.(js|jsx|ts|tsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"],
+					},
+				},
+			},
+			{
+				// Add a rule for TypeScript files
+				test: /\.ts$/,
+				exclude: /node_modules/,
+				use: "ts-loader",
+			},
+			{
+				test: /\.css$/,
+				exclude: /node_modules/,
+				use: ["style-loader", "css-loader", "postcss-loader"],
+			},
+			{
+				test: /\.html$/i,
+				loader: "html-loader",
+			},
+			{
+				test: /\.(png|jpe?g|ico|svg|webp)$/,
+				type: "asset/resource",
+				generator: {
+					filename: "assets/images/[name][ext]",
+				},
+			},
+		],
+	},
+	plugins: [
+		new webpack.ProgressPlugin(),
+		new HtmlWebpackPlugin({
+			title: "Webpack App",
+			filename: "index.html",
+			template: "src/index.html",
+		}),
+	],
+	devtool: "source-map",
+	devServer: {
+		port: 3000, // customize the port
+		static: {
+			directory: path.resolve(__dirname, "src"),
+		},
+		hot: true, // hot reloading
+		open: true, // open the page when server starts
+		compress: true, // enable gzip compression
+	},
+	// usedExport enables tree-shaking feature
+	optimization: {
+		usedExports: true,
+		chunkIds: "named",
+	},
+	performance: {
+		hints: false,
+		maxEntrypointSize: 512000,
+		maxAssetSize: 512000,
+	},
+};
+
+module.exports = webpackConfig;
